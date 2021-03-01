fe1b3f79d05521b51526d1b830888fad89afa7cd
sheryltsz <70138510+sheryltsz@users.noreply.github.com>
2021-03-01 18:43:25 +0800

    insertgame

---
+++ Assets/Script/OptionButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionButtonScript : MonoBehaviour
+{
+    public Button yourButton;
+
+    GameManagerScript gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        gm = FindObjectOfType<GameManagerScript>();
+        gm.responded = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick()
+    {
+        gm.responded = true;
+        int.TryParse(this.name.Substring(name.Length - 1, 1), out gm.btnSelected);
+        Debug.Log(gm.btnSelected);
+    }
+
+}
